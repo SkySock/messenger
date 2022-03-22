cd68eb604a4722a1fa59fb201bfb4bd3ecab9a1c
SkySock <inikitenko@list.ru>
2022-03-22 21:33:11 +0500

    update UserSerializer

--- src/account/serializers.py
+++ src/account/serializers.py
@@ -1,13 +1,24 @@
 from attr import field
 from rest_framework import serializers
-
+from src.followers.models import UserFollowing
 from .models import AuthUser
         
         
 class UserSerializer(serializers.ModelSerializer):
     profile_image = serializers.ImageField(read_only=True)
+    is_followed = serializers.SerializerMethodField('get_is_followed')
     
     class Meta:
         model = AuthUser
-        fields = ("id", "username", "display_status", "profile_image",)
+        fields = ("id", "username", "display_status", "profile_image", 'is_followed')
+
+    def get_is_followed(self, obj):
+        request = self.context.get('request')
+        if not request:
+            return None
+        try:
+            UserFollowing.objects.get(user=request.user, following_user=obj)
+        except UserFollowing.DoesNotExist:
+            return False
+        return True
     
